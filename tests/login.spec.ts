932ee65dbe9f8ba8df4c6ae7d4fa4470bbb8aaa3
Marcin <marcin.bubolc@gmail.com>
2024-01-22 23:47:40 +0100

    tests refactor

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,14 +1,13 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('User login to Demobank', () => {
+  // Arrange
   const url = 'https://demo-bank.vercel.app/';
   const userLogin = 'testerLO';
   const userPassword = 'test1234';
   const expectedUserName = 'Jan Demobankowy';
 
   test('login with correct credentials', async ({ page }) => {
-    // Arrange
-
     // Act
     await page.goto(url);
     await page.getByTestId('login-input').fill(userLogin);
@@ -19,23 +18,35 @@ test.describe('User login to Demobank', () => {
   });
 
   test('unsuccessful login with too short login', async ({ page }) => {
-    await page.goto(url);
-    await page.getByTestId('login-input').fill('tester');
-    await page.getByTestId('password-input').click();
+    //Arrange
+    const expectedErrorMessage = 'identyfikator ma min. 8 znaków';
+    const incorrectUsername = 'tester';
 
+    //Act
+    await page.goto(url);
+    await page.getByTestId('login-input').fill(incorrectUsername);
+    await page.getByTestId('password-input').click();
+    
+    //Assert
     await expect(page.getByTestId('error-login-id')).toHaveText(
-      'identyfikator ma min. 8 znaków',
+      expectedErrorMessage,
     );
   });
 
   test('unsuccessful login with too short password', async ({ page }) => {
+    //Arrange
+    const expectedErrorMessage = 'hasło ma min. 8 znaków';
+    const incorrectPassword = 'test';
+
+    //Act
     await page.goto(url);
     await page.getByTestId('login-input').fill(userLogin);
-    await page.getByTestId('password-input').fill('test');
+    await page.getByTestId('password-input').fill(incorrectPassword);
     await page.getByTestId('password-input').blur();
 
+    //Assert
     await expect(page.getByTestId('error-login-password')).toHaveText(
-      'hasło ma min. 8 znaków',
+      expectedErrorMessage,
     );
   });
 });
--- tests/pulpit.spec.ts
+++ tests/pulpit.spec.ts
@@ -1,16 +1,18 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Pulpit tests', () => {
-  test.only('Quick payment with correct data', async ({ page }) => {
-    // Arrange
-    const url = 'https://demo-bank.vercel.app/';
-    const userLogin = 'testerLO';
-    const userPassword = 'test1234';
-    const receiverId = '2';
-    const transferAmount = '150';
-    const transferTitle = 'pizza';
-    const expectedTransferReceiver = 'Chuck Demobankowy';
+  // Arrange
+  const url = 'https://demo-bank.vercel.app/';
+  const userLogin = 'testerLO';
+  const userPassword = 'test1234';
+  const receiverId = '2';
+  const transferAmount = '150';
+  const transferTitle = 'pizza';
+  const expectedTransferReceiver = 'Chuck Demobankowy';
+  const topUpPhone = '502 xxx xxx';
+  const topupAmount = '50';
 
+  test('Quick payment with correct data', async ({ page }) => {
     // Act
     await page.goto(url);
     await page.getByTestId('login-input').fill(userLogin);
@@ -20,31 +22,35 @@ test.describe('Pulpit tests', () => {
     await page.locator('#widget_1_transfer_amount').fill(transferAmount);
     await page.locator('#widget_1_transfer_title').fill(transferTitle);
 
-    // Assert
     await page.getByRole('button', { name: 'wykonaj' }).click();
     await page.getByTestId('close-button').click();
-
+    
+    // Assert
     await expect(page.locator('#show_messages')).toHaveText(
       `Przelew wykonany! ${expectedTransferReceiver} - ${transferAmount},00PLN - ${transferTitle}`,
     );
   });
 
   test('Successful mobile top-up', async ({ page }) => {
-    await page.goto('https://demo-bank.vercel.app/index.html');
-    await page.getByTestId('login-input').fill('login123');
-    await page.getByTestId('password-input').fill('password');
+    // Arrange
+    const expectedMessage = `Doładowanie wykonane! ${topupAmount},00PLN na numer ${topUpPhone}`;
+
+    // Act
+    await page.goto(url);
+    await page.getByTestId('login-input').fill(userLogin);
+    await page.getByTestId('password-input').fill(userPassword);
     await page.getByTestId('login-button').click();
 
-    await page.locator('#widget_1_topup_receiver').selectOption('502 xxx xxx');
-    await page.locator('#widget_1_topup_amount').fill('50');
+    await page.locator('#widget_1_topup_receiver').selectOption(topUpPhone);
+    await page.locator('#widget_1_topup_amount').fill(topupAmount);
     await page.locator('#uniform-widget_1_topup_agreement').click();
 
     await page.getByRole('button', { name: 'doładuj telefon' }).click();
     await page.getByTestId('close-button').click();
 
+    // Assert
     await expect(page.locator('#show_messages')).toHaveText(
-      'Doładowanie wykonane! 50,00PLN na numer 502 xxx xxx',
+      expectedMessage,
     );
-    console.log('Testowa treść do zalogowania w konsoli');
   });
 });
